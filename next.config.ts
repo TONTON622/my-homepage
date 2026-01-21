99d738229c3fefcb218cfdbdd9cee09c5ae17e07
TONTON622 <riki622tr@gmail.com>
2026-01-22 05:25:23 +0900

    config修正

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // これを追加！
+  images: {
+    unoptimized: true, // これも追加！
+  },
 };
 
 export default nextConfig;
